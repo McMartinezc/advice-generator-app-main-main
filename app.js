37f0a34573f52389cdb0ad2b1d1e1cad36b8353b
McMartinezc <psyence_mc@hotmail.com>
2023-10-31 09:40:21 +0100

    ok

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // ¡Buena suerte!
+const button = document.querySelector(".card__dice");
+const textContainer = document.querySelector(".card__quote");
+
+
+button.addEventListener("click", async()=>{
+    const advice = await getRandomAdvice();
+    textContainer.textContent = advice;
+})
+
+
+//Función fetch
+async function getRandomAdvice(){
+    const urlApi= "https://api.adviceslip.com/advice";
+    const response = await fetch(urlApi);
+    const data = await response.json();
+    return data.slip.advice;
+}
